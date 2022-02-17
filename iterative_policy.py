3107d3de65cf2c9e84945e80c73a2c6db34fadff
lanearohan <lanearohan@gmail.com>
2022-02-17 11:01:10 -0600

    Setup commit

---
+++ iterative_policy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def iterative_policy(): 
+
+    print("this works")
+
+    # create a 2D array for the state grid [rows][columns]
+    stategrid = np.zeros((4,4))
+
+    print(stategrid)
+
+
+
+# RUNNING
+# to run:
+# 1. check to see if python3 is installed with 
+#      python3 --version
+# 2. run command `python3 iterative_policy.py` 
+iterative_policy()
